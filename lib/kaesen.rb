d1dd41dfbfa98da9d6062a13b68c6a78b70fffa3
TYoko <tetsuo-jp@users.noreply.github.com>
2016-03-10 19:51:12 +0900

    LakeBTC (ticker, depth) added.

--- lib/kaesen.rb
+++ lib/kaesen.rb
@@ -5,6 +5,7 @@ require "kaesen/coincheck"
 require "kaesen/zaif"
 require "kaesen/monetago"
 require "kaesen/kraken"
+require "kaesen/lakebtc"
 require "kaesen/client"
 
 # A module of the united wrapper for exchanging Japanese yen and Bitcoin and
