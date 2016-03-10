2e0f8494ca0970591d23dcd29fadb96df6748ebe
TYoko <tetsuo-jp@users.noreply.github.com>
2016-03-10 19:38:49 +0900

    Quoine (ticker, depth) added.

--- lib/kaesen.rb
+++ lib/kaesen.rb
@@ -5,6 +5,7 @@ require "kaesen/coincheck"
 require "kaesen/zaif"
 require "kaesen/monetago"
 require "kaesen/kraken"
+require "kaesen/quoine"
 require "kaesen/client"
 
 # A module of the united wrapper for exchanging Japanese yen and Bitcoin and
